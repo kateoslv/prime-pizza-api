4c96a4b2c62e5e9cf5b6bc7ca6d98d32b5775dfa
katty oliveira <kattyolv@gmail.com>
2020-12-03 17:10:17 +0000

    created project and added connection JDBC class

---
+++ src/com/kattyolv/prime/pizza/api/jdbc/ConnectionJDBC.java
@@ -0,0 +1,40 @@
+package com.kattyolv.prime.pizza.api.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+	
+	private Connection connection = null;
+	private static final String USER = "root";
+	private static final String PASSWORD = "123456*";
+	private static final String URL = "jdbc:mysql://localhost:3306/prime_pizza_api";
+	
+	public Connection getConnection() {
+		
+		if (connection == null) {
+			try {
+				connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			} 
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return connection;
+	}
+	
+	public void closeConnection() {
+		
+		if (connection != null) {
+			try {
+				connection.close();
+			} 
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
